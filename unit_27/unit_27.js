f065d3644fba50d0a467abb913a4734de0509bd5
Vitali Makal <vmakal1989@gmail.com>
2020-11-03 12:46:52 +0300

    unit_27

---
+++ unit_27/unit_27.js
@@ -0,0 +1,196 @@
+const myRequest = (method, url, body) => {
+	return new Promise((resolve, reject) => {
+		fetch(url, {
+			method: method, // *GET, POST, PUT, DELETE, etc.
+			mode: 'cors', // no-cors, *cors, same-origin
+			cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+			credentials: 'same-origin', // include, *same-origin, omit
+			headers: {
+				//'Content-Type': 'application/json'
+				'Content-Type': 'application/x-www-form-urlencoded',
+			},
+			redirect: 'follow', // manual, *follow, error
+			referrerPolicy: 'no-referrer', // no-referrer, *client
+			body: body // body data type must match "Content-Type" header
+		})
+			.then(response => resolve(response.text()));
+	})
+}
+
+const allPromises = (p1, p2, out) => {
+	Promise.all([p1,p2]).then (response => {
+		document.querySelector(out).innerHTML = `${response[0]} <br> ${response[1]}`
+	})
+}
+
+const key = 'auth=5ADcB96BA48d3f80';
+const url = `http://getpost.itgid.info/index2.php`;
+// Task 1 ============================================
+/* 
+ <p>Отправьте GET запрос на сайт http://getpost.itgid.info/index2.php. В качестве action укажите 1. </p>
+<p>Отправьте GET запрос на сайт http://getpost.itgid.info/index2.php. В качестве action укажите 2. </p>
+<p>Два запроса объедините с помощью promiseAll. Результат выведите в out-1 результат. Запускаться функция
+    должна по нажатию b-1.</p>
+*/
+
+function t1() {
+	let p1 = myRequest('GET', `${url}?${key}&action=1`);
+	let p2 = myRequest('GET', `${url}?${key}&action=2&name=Vitali`);
+	allPromises(p1,p2, '.out-1');
+}
+
+// ваше событие здесь!!!]
+document.querySelector('.b-1').onclick = t1;
+
+// Task 2 ============================================
+/* 
+ <p> Отправьте GET запрос на сайт http://getpost.itgid.info/index2.php. В качестве action укажите 3. Добавьте
+параметр num1 и num2 содержащие числа. Если все сделано верно, сервер вернет сумму чисел.</p>
+<p>Отправьте GET запрос на сайт http://getpost.itgid.info/index2.php. В качестве action укажите 4.
+Добавьте параметр num1 и num2 содержащие числа. Если все сделано верно, сервер вернет случайное число в
+заданном
+диапазоне.</p>
+<p>Два запроса объедините с помощью promiseAll.
+Выведите в out-2 результат. Запускаться функция должна по нажатию b-2. </p>
+
+*/
+
+function t2() {
+	let p1 = myRequest('GET', `${url}?${key}&action=3&num1=2&num2=25`);
+	let p2 = myRequest('GET', `${url}?${key}&action=4&num1=3&num2=43`);
+	allPromises(p1,p2, '.out-2');
+}
+
+// ваше событие здесь!!!
+document.querySelector('.b-2').onclick = t2;
+
+
+// Task 3 ============================================
+/*  
+<p> Отправьте GET запрос на сайт http://getpost.itgid.info/index2.php. В качестве action укажите 5.
+Если все сделано верно, сервер вернет текущее время и дату. Не забывайте указывать параметр auth (ключ в
+чате). </p>
+<p> Отправьте GET запрос на сайт http://getpost.itgid.info/index2.php. В качестве action укажите 6.
+Добавьте
+параметр num1 и num2 содержащие числа. Если все сделано верно, сервер вернет большее число.</p>
+<p>Два
+запроса объедините с помощью promiseAll.
+Выведите в out-3 результат. Запускаться функция должна по нажатию b-3. </p>
+                 */
+
+function t3() {
+	let p1 = myRequest('GET', `${url}?${key}&action=5`);
+	let p2 = myRequest('GET', `${url}?${key}&action=6&num1=32&num2=225`);
+	allPromises(p1,p2, '.out-3');
+}
+
+// ваше событие здесь!!!
+document.querySelector('.b-3').onclick = t3;
+
+// Task 4 ============================================
+/*  
+ <p> Отправьте GET запрос на сайт http://getpost.itgid.info/index2.php. В качестве action укажите 7.
+Если все
+сделано верно, сервер случайную ссылку на изображение. Не забывайте указывать параметр auth (ключ в
+чате). </p>
+<p>Отправьте GET запрос на сайт http://getpost.itgid.info/index2.php. В качестве action укажите 8. В
+качестве параметра по очереди укажите year равный году вашего рождения. Если все правильно сервер вернет
+ваш возраст.</p>
+<p>Выведите в out-4 результат. Запускаться функция должна по нажатию b-4.</p>
+
+*/
+
+function t4() {
+	let p1 = myRequest('GET', `${url}?${key}&action=7`);
+	let p2 = myRequest('GET', `${url}?${key}&action=8&year=1989`);
+	allPromises(p1,p2, '.out-4');
+}
+
+// ваше событие здесь!!!
+document.querySelector('.b-4').onclick = t4;
+
+// Task 5 ============================================
+/*  
+ <p>Отправьте POST запрос на сайт http://getpost.itgid.info/index2.php. В качестве action укажите 1.</p>
+<p>Отправьте
+POST запрос на сайт http://getpost.itgid.info/index2.php. В качестве action укажите 2. </p>
+<p>Два
+запроса объедините с помощью promiseAll. Результат выведите в out-5 результат. Запускаться функция
+должна по нажатию b-5.</p>
+*/
+
+function t5() {
+	let p1 = myRequest('POST', url, `${key}&action=1&y=1`);
+	let p2 = myRequest('POST', url, `${key}&action=2&name=Vitali`);
+	allPromises(p1,p2, '.out-5');
+}
+
+// ваше событие здесь!!!
+document.querySelector('.b-5').onclick = t5;
+
+// Task 6 ============================================
+/* 
+ <p> Отправьте POST запрос на сайт http://getpost.itgid.info/index2.php. В качестве action укажите 3.
+    Добавьте
+    параметр num1 и num2 содержащие числа. Если все сделано верно, сервер вернет сумму чисел. </p>
+<p>Отправьте POST
+    запрос на сайт http://getpost.itgid.info/index2.php. В качестве action укажите 4.
+    Добавьте параметр num1 и num2 содержащие числа. Если все сделано верно, сервер вернет случайное число в
+    заданном
+    диапазоне.</p>
+<p> Два запроса объедините с помощью promiseAll.
+    Выведите в
+    out-6 результат. Запускаться функция должна по нажатию b-6. </p>
+*/
+
+function t6() {
+	let p1 = myRequest('POST', url, `${key}&action=3&num1=3&num2=324`);
+	let p2 = myRequest('POST', url,`${key}&action=4&num1=23&num2=34`);
+	allPromises(p1,p2, '.out-6');
+}
+
+// ваше событие здесь!!!
+document.querySelector('.b-6').onclick = t6;
+
+// Task 7 ============================================
+/*  
+ <p> Отправьте POST запрос на сайт http://getpost.itgid.info/index2.php. В качестве action укажите 5.
+Если все сделано верно, сервер вернет текущее время и дату. Не забывайте указывать параметр auth (ключ в
+чате).</p>
+<p>Отправьте POST запрос на сайт http://getpost.itgid.info/index2.php. В качестве action укажите 6.
+Добавьте параметр num1 и num2 содержащие числа. Если все сделано верно, сервер вернет большее число.</p>
+<p>Два запроса объедините с помощью promiseAll.
+Выведите в out-7 результат. Запускаться функция должна по нажатию b-7. </p>
+
+*/
+
+function t7() {
+	let p1 = myRequest('POST', url, `${key}&action=5&`);
+	let p2 = myRequest('POST', url, `${key}&action=6&num1=7&num2=18`);
+	allPromises(p1,p2, '.out-7');
+}
+
+// ваше событие здесь!!!
+document.querySelector('.b-7').onclick = t7;
+
+// Task 8 ============================================
+/* 
+<p> Отправьте POST запрос на сайт http://getpost.itgid.info/index2.php. В качестве action укажите 7.
+Если все сделано верно, сервер случайную ссылку на изображение. Не забывайте указывать параметр auth
+(ключ в
+чате).</p>
+<p>Отправьте POST запрос на сайт http://getpost.itgid.info/index2.php. В качестве action укажите 8. В
+качестве параметра по очереди укажите year равный году вашего рождения. Если все правильно сервер вернет
+ваш возраст.</p>
+<p>Два запроса объедините с помощью promiseAll. Выведите в out-8 результат. Запускаться функция должна по
+нажатию b-8.</p>
+*/
+
+function t8() {
+	let p1 = myRequest('POST', url, `${key}&action=7&`);
+	let p2 = myRequest('POST', url, `${key}&action=8&year=1989`);
+	allPromises(p1,p2, '.out-8');}
+
+// ваше событие здесь!!!
+document.querySelector('.b-8').onclick = t8;
+
